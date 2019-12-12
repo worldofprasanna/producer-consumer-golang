e07361962597f10da8a797cee26169bb5365c2ac
Prasanna V <prasannacse2005@gmail.com>
2019-12-12 21:35:05 +0530

    Add wait group in main go routine

--- main.go
+++ main.go
@@ -1,28 +1,33 @@
 package main
 
 import (
+	"sync"
 	"fmt"
-	"time"
 )
 
-func producer(queue chan string) {
+func producer(queue chan string, waitGroup *sync.WaitGroup) {
 	for i := 0; i < 10; i++ {
 		fmt.Println("producing the item: ", (i+1))
 		queue <- fmt.Sprintf("item: %d", (i+1))
 	}
+	close(queue)
+	waitGroup.Done()
 }
 
-func consumer(queue chan string) {
+func consumer(queue chan string, waitGroup *sync.WaitGroup) {
 	for val := range queue {
 		fmt.Println("consuming the ", val)
 	}
+	waitGroup.Done()
 }
 
 func main() {
 	fmt.Println("Yet Another Producer, Consumer Solution !!! YAPCS")
 	queue := make(chan string)
-	go producer(queue)
-	go consumer(queue)
-	time.Sleep(20 * time.Millisecond)
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(2)
+	go producer(queue, &waitGroup)
+	go consumer(queue, &waitGroup)
+	waitGroup.Wait()
 	fmt.Println("-------------------------------------------------")
 }
