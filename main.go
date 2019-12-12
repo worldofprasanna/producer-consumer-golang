43487156370f8e73232840b716f942369a93b10f
Prasanna V <prasannacse2005@gmail.com>
2019-12-12 20:45:31 +0530

    Introduce channels and sleep in main goroutine

--- main.go
+++ main.go
@@ -5,21 +5,24 @@ import (
 	"time"
 )
 
-func producer() {
+func producer(queue chan string) {
 	for i := 0; i < 10; i++ {
-		time.Sleep(time.Millisecond)
-		fmt.Println("producing the item ", i)
+		fmt.Println("producing the item: ", (i+1))
+		queue <- fmt.Sprintf("item: %d", (i+1))
 	}
 }
 
-func consumer() {
+func consumer(queue chan string) {
+	for val := range queue {
+		fmt.Println("consuming the ", val)
+	}
 }
 
 func main() {
 	fmt.Println("Yet Another Producer, Consumer Solution !!! YAPCS")
-	// Invoke producer in a separate go routine
-	go producer()
-	// Invoke consumer in a separate go routine
-	go consumer()
+	queue := make(chan string)
+	go producer(queue)
+	go consumer(queue)
+	time.Sleep(20 * time.Millisecond)
 	fmt.Println("-------------------------------------------------")
 }
